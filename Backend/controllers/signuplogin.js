4fd54b19c1bcd82e7e7394ca4737f033eae4ebad
Samarth <sssamarth818@gmail.com>
2022-10-11 23:03:56 +0530

    Adding Login Backend with json web Token

--- Backend/controllers/signuplogin.js
+++ Backend/controllers/signuplogin.js
@@ -1,5 +1,7 @@
 let bcrypt = require("bcrypt");
 let User = require("../models/user");
+let jwt =require('jsonwebtoken');
+let key ="secretkey12";
 
 exports.postsignup = async (req, res) => {
   let { name, email, phonenumber, pwd } = req.body;
@@ -20,4 +22,27 @@ exports.postsignup = async (req, res) => {
     });
 };
 
+exports.login=async (req,res,next)=>{
+  let { email, pwd } = req.body;
 
+  let user = await User.findAll({ where: { email: email } });
+
+  if (user.length > 0) {
+    const dbid = user[0].id;
+    const dbemail = user[0].email;
+    const dbpassword = user[0].password;
+    const dbname = user[0].name;
+    const dbphonenumber = user[0].phonenumber;
+
+    const emailpwdmatch = await bcrypt.compare(pwd, dbpassword);
+
+    if (emailpwdmatch) {
+      const token = jwt.sign(dbid, key);
+      res.status(200).json({ msg: "login successful", token: token });
+    } else {
+      res.status(401).json({ msg: "something went wrong" });
+    }
+  } else {
+    res.status(404).json({ msg: "user not found" });
+  }
+};
--- Backend/routes/loginsignup.js
+++ Backend/routes/loginsignup.js
@@ -3,5 +3,6 @@ let router = express.Router();
 let Controllers = require("../controllers/signuplogin");
 
 router.post("/userregister", Controllers.postsignup);
+router.post('/userlogin',Controllers.login)
 
 module.exports=router;
